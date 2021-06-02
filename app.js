e2ce9e0dfd2ba12e7d4d8b68702acd7ab2bc509b
g <umacgamze49@gmail.com>
2021-06-02 11:01:05 +0200

    dynamich-classes

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 
 const app = Vue.createApp({
-    data() {
-      return {
-        url:'',
-        showBooks: true,
-        books: [
-          { title: 'name of the wind', 
+  data() {
+    return {
+      url: '',
+      showBooks: true,
+      books: [
+        {
+          title: 'name of the wind',
           author: 'patrick rothfuss',
-          img:'assets/1.jpg'},
-          { title: 'the way of kings', 
+          img: 'assets/1.jpg',
+          isFav: true
+        },
+        {
+          title: 'the way of kings',
           author: 'brandon sanderson',
-          img:'assets/2.jpg' },
-          { title: 'the final empire', 
+          img: 'assets/2.jpg',
+          isFav: false
+        },
+        {
+          title: 'the final empire',
           author: 'brandon sanderson',
-          img:'assets/3.jpg' }
-        ],
-      }
-    },
-    methods: {
-      toggleShowBooks() {
-        this.showBooks =  !this.showBooks
-      },
+          img: 'assets/3.jpg',
+          isFav: true
+        }
+      ],
     }
-  })
-  
-  app.mount('#app')
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks
+    },
+    toggleFav(book){
+      book.isFav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks(){
+      return this.books.filter((book) => book.isFav)
+    }
+  }
+})
+
+app.mount('#app')
